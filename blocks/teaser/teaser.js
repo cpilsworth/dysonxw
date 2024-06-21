88388745069deda01b4a4746c557143bf78ba70e
Chris Pilsworth <chrisp@adobe.com>
2024-06-21 23:51:13 +0100

    teaser

---
+++ blocks/teaser/teaser.js
@@ -0,0 +1,16 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default function decorate(block) {
+//   let image = block.querySelector(':scope > div > div:first-child');
+//   image.className = 'teaser-image';
+  
+  // let image = block.querySelector(':scope > div * div');
+  // for (let i = 0; i < image.children.length; i++) {
+  //   let img = image.children[i];
+  //   console.log(img);
+  //   img.className = (i === 0) ? 'teaser-image' : 'teaser-content';
+  // }
+
+//   let content = block.querySelector(':scope > div > div:nth-child(2)');
+//   content.className = 'teaser-content';
+}
